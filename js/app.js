a3a2c32728aa93291982276a7986d804151626a5
papanugget <evilgenius04@gmail.com>
2018-11-03 12:18:04 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// document.getElementById('greeting').innerHTML = "Hello from vanilla JS";
+
+// $('#greeting').text('Hello from jQuery')
+
+new Vue({
+    el: '#app',
+    data: {
+        greeting: "Hello from Vue"
+    }
+});
