2d4ff51f4479aa53c9273a281aabaf7ef59d06db
papanugget <evilgenius04@gmail.com>
2018-11-04 16:40:04 -0500

    loading data from api

--- js/app.js
+++ js/app.js
@@ -8,18 +8,35 @@
 //         greeting: "Hello from Vue"
 //     }
 // });
+const apiKey = 'z0YJP1kJd0Jg3INdrVgZffIc8CF7mDB1vVsqIPFw';
+const url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
 
+// const vm = new Vue({
+//     el: '#app',
+//     data: {
+//         email: 'grep@aol.com',
+//         submitted: false
+//     },
+//     methods: {
+//         process: function(e) {
+//             // e.preventDefault();
+//             // alert('Submitted' + this.email);
+//             this.submitted = true;
+//         }
+//     }
+// });
 const vm = new Vue({
     el: '#app',
     data: {
-        email: 'grep@aol.com',
-        submitted: false
-    },
-    methods: {
-        process: function(e) {
-            // e.preventDefault();
-            // alert('Submitted' + this.email);
-            this.submitted = true;
-        }
+        imgSrc: '',
+        imgTitle: '',
+        imgAlt: '',
+        explanation: ''
     }
 });
+axios.get(url)
+    .then(res => {
+        vm.imgSrc = res.data.url,
+        vm.imgTitle = res.data.title
+        vm.explanation = res.data.explanation;
+    });
