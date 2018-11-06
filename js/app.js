3ecea80a6c6bb4059ea33424b9b7c2f5b6c9368b
papanugget <evilgenius04@gmail.com>
2018-11-06 12:41:15 -0500

    added lifecycle method

--- js/app.js
+++ js/app.js
@@ -8,8 +8,7 @@
 //         greeting: "Hello from Vue"
 //     }
 // });
-const apiKey = 'z0YJP1kJd0Jg3INdrVgZffIc8CF7mDB1vVsqIPFw';
-const url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
+
 
 // const vm = new Vue({
 //     el: '#app',
@@ -32,13 +31,29 @@ const vm = new Vue({
         imgTitle: '',
         imgAlt: '',
         explanation: ''
+    },
+    created: function() {
+        this.fetchAPod();
+    },
+    methods: {
+        fetchAPod: function() {
+            const apiKey = 'z0YJP1kJd0Jg3INdrVgZffIc8CF7mDB1vVsqIPFw';
+            const url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
+            axios.get(url)
+            .then(res => {
+                vm.imgSrc = res.data.url,
+                vm.imgTitle = res.data.title
+                vm.explanation = res.data.explanation;
+            });
+        }
     }
 });
-axios.get(url)
-    .then(res => {
-        vm.imgSrc = res.data.url,
-        vm.imgTitle = res.data.title
-        vm.explanation = res.data.explanation;
-    });
 
-    
+// moved to methods object in VM
+// axios.get(url)
+//     .then(res => {
+//         vm.imgSrc = res.data.url,
+//         vm.imgTitle = res.data.title
+//         vm.explanation = res.data.explanation;
+//     });
+
